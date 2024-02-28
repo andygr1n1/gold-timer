15ba737813ddd6dbac6bf798c04647be8acbaf19
Grini Andrei <andrei.grini@carasent.com>
2024-02-28 21:39:45 +0000

    feat: updated crypto bubbles

--- src/modules/crypto/CryptoIndex.tsx
+++ src/modules/crypto/CryptoIndex.tsx
@@ -6,9 +6,7 @@ export const CryptoIndex: React.FC = () => {
     return (
         <ModuleWrapper context={APP_ROUTES_ENUM.CRYPTO}>
             <object className='h-[calc(100vh-190px)] min-h-[700px] w-full' data={cryptoBubblesLink()} />
-            <div className='font bold text-cText'>
-                Currencies: {cryptoBubblesCount()} - <span className='text-blue-500'>24</span>
-            </div>
+            <div className='font bold text-cText'>Currencies: {cryptoBubblesCount()}</div>
         </ModuleWrapper>
     )
 }
