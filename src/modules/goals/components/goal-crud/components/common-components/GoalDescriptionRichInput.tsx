16c880fc68e7072fbda32b1e811ddb491e1ed659
Grini Andrei <andrei.grini@carasent.com>
2024-02-29 15:36:16 +0000

    GoalDescriptionRichInput fixed linebreaking

--- src/modules/goals/components/goal-crud/components/common-components/GoalDescriptionRichInput.tsx
+++ src/modules/goals/components/goal-crud/components/common-components/GoalDescriptionRichInput.tsx
@@ -1,6 +1,6 @@
 import { FormLabel } from '@/components/form/FormLabel'
 import { cn } from '@/functions/helpers'
-import { observer } from 'mobx-react-lite'
+import { useEffect, useState } from 'react'
 import ReactQuill from 'react-quill'
 
 export const GoalDescriptionRichInput: React.FC<{
@@ -8,15 +8,23 @@ export const GoalDescriptionRichInput: React.FC<{
     hide?: boolean
     view_mode?: boolean
     onChange?: (value: string) => void
-}> = observer(({ value = '', hide = false, view_mode = false, onChange }) => {
+}> = ({ value = '', hide = false, view_mode = false, onChange }) => {
     if (view_mode && !value.length) return null
+
+    /* Fixing ReactQuill  */
+    /* https://github.com/zenoamaro/react-quill/issues/911 */
+    /* In react-quill text editor move next line using Enter kay, need to press Enter kay two time. */
+    const [val, setVal] = useState('')
+    useEffect(() => {
+        setVal(value)
+    }, [hide])
 
     return !hide ? (
         <div>
             <FormLabel title='Description' />
             <ReactQuill
                 className={cn(view_mode && 'view-mode')}
-                value={value}
+                value={view_mode ? val : value}
                 onChange={(content) => {
                     onChange?.(content)
                 }}
@@ -25,4 +33,4 @@ export const GoalDescriptionRichInput: React.FC<{
             />
         </div>
     ) : null
-})
+}
