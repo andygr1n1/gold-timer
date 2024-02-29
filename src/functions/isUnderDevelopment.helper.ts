a3799fb8060e5c670ce45e551dc384e6fec6d89b
Grini Andrei <andrei.grini@carasent.com>
2024-02-29 14:37:02 +0000

    feat: Foxy open kzen

--- src/functions/isUnderDevelopment.helper.ts
+++ src/functions/isUnderDevelopment.helper.ts
@@ -3,5 +3,9 @@ import { rootStore$ } from '@/StoreProvider'
 export const isDev = () => import.meta.env.VITE_NODE_ENV === 'prod'
 
 export const isUnderDevelopment = (): boolean => {
-    return isDev() || rootStore$.user$.id === 'f192b78e-399e-4fc5-9676-ce0bf65b220b'
+    return (
+        isDev() ||
+        rootStore$.user$.id === 'f192b78e-399e-4fc5-9676-ce0bf65b220b' ||
+        rootStore$.user$.id === 'c82d3a82-9cc4-48a5-9fc3-c78fd09a6c1b'
+    )
 }
