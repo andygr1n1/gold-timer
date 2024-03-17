0d06936d0bc6d3edafa2778a4b43d279661fd113
Grini Andrei <andrei.grini@carasent.com>
2024-03-17 20:27:31 +0000

    feat: Profile avatar refacktoring

--- src/components/side-menu/components/user-info/UserInfo.tsx
+++ src/components/side-menu/components/user-info/UserInfo.tsx
@@ -6,14 +6,15 @@ import { APP_ROUTES_ENUM } from '@/helpers/enums'
 import { useSideMenu } from '@/hooks/useSideMenu.hook'
 
 export const UserInfo: React.FC = () => {
-    const { isLoading, name, avatar } = useFetchAvatar()
+    const { isLoading, name } = useFetchAvatar()
+
     return (
         <div className='min-h-[220px]'>
             {!isLoading && (
                 <div className='animate-opacity-3 relative flex min-h-[220px] flex-col items-center justify-center gap-5'>
                     <UserName name={name} />
                     <NavLink to={APP_ROUTES_ENUM.PROFILE} onClick={() => useSideMenu.is_open && useSideMenu.onChange()}>
-                        <ProfileAvatar avatar={avatar} />
+                        <ProfileAvatar />
                     </NavLink>
                 </div>
             )}
