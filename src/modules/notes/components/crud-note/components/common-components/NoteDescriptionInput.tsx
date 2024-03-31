6bf173a1a4873a6b59fa7f7cc10d0b4f8398df0f
Grini Andrei <andrei.grini@carasent.com>
2024-03-31 17:12:30 +0100

    unified xrte

--- src/modules/notes/components/crud-note/components/common-components/NoteDescriptionInput.tsx
+++ src/modules/notes/components/crud-note/components/common-components/NoteDescriptionInput.tsx
@@ -1,9 +1,9 @@
+import { XRte } from '@/components-x/x-rte/XRte'
 import { FormLabel } from '@/components/form/FormLabel'
 import { INote$ } from '@/modules/notes/mst/types'
 import { Form } from 'antd'
 import { observer } from 'mobx-react-lite'
 import { useEffect } from 'react'
-import ReactQuill from 'react-quill'
 
 export const NoteDescriptionInput: React.FC<{ note: INote$ }> = observer(({ note }) => {
     const { description, onChangeField } = note
@@ -15,26 +15,10 @@ export const NoteDescriptionInput: React.FC<{ note: INote$ }> = observer(({ note
     return (
         <Form.Item>
             <FormLabel title='Note:' />
-            <ReactQuill
-                preserveWhitespace
-                value={description}
-                onChange={(content) => {
+            <XRte
+                content={description}
+                onChangeContent={(content) => {
                     onChangeField('description', content)
-                }}
-                placeholder='Note...'
-                className='[&_.ql-editor]:!max-h-[130px] [&_.ql-editor]:!min-h-[130px]'
-                formats={['bold', 'italic', 'underline', 'strike', 'color', 'link', 'image', 'clean']}
-                modules={{
-                    toolbar: [
-                        'bold',
-                        'italic',
-                        'underline',
-                        'strike',
-                        { color: ['red', 'blue', 'green'] },
-                        'link',
-                        'image',
-                        'clean',
-                    ],
                 }}
             />
         </Form.Item>
