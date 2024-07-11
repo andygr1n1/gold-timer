7d7f2077098a82b7f5b4ba1f80bb28b04f4e75dd
Grini Andrei <andrei.grini@carasent.com>
2024-07-11 19:12:53 +0100

    feat: Optimizations

--- src/modules/app/App.tsx
+++ src/modules/app/App.tsx
@@ -3,12 +3,9 @@ import { useJwtAuth } from './hooks/useJwtAuth.hook'
 import { useTheming } from './hooks/useTheming.hook'
 import { AppRoutes } from './AppRoutes'
 import { ConfigProvider } from 'antd'
-import { useAtom } from 'jotai'
-import { darkModeAtom } from '@/components/side-menu/stores/themingStore'
 
 export const App = () => {
     useTheming()
-    const [isDarkMode] = useAtom(darkModeAtom)
     const { isLoading } = useJwtAuth()
 
     if (isLoading) return <div className='authorization-page' />
@@ -17,7 +14,8 @@ export const App = () => {
         <ConfigProvider
             theme={{
                 token: {
-                    colorText: isDarkMode ? '#FFFFFF' : '#000000',
+                    colorPrimary: 'var(--colors-cText)',
+                    colorText: 'var(--colors-cText)',
                     fontSize: 16,
                     fontFamily: 'Inter. sans-serif',
                     colorBgContainer: 'var(--colors-global-bg)',
