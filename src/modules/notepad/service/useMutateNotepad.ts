3292910a8082cd3252773a117f4cac0a65a8cb15
Grini Andrei <andrei.grini@carasent.com>
2024-08-22 21:52:56 +0300

    fixed notepad data fetching and mutation

--- src/modules/notepad/service/useMutateNotepad.ts
+++ src/modules/notepad/service/useMutateNotepad.ts
@@ -1,8 +1,19 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { mutation_notepadDescription } from './mutation_notepadDescription'
+import { KEY_FetchNotepad } from './keys'
 
 export const useMutateNotepad = () => {
-    return useMutation({
+    const queryClient = useQueryClient()
+    const mutation = useMutation({
         mutationFn: ({ description }: { description: string }) => mutation_notepadDescription(description),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: KEY_FetchNotepad() })
+        },
     })
+
+    const updateDescription = ({ description }: { description: string }) => {
+        return mutation.mutate({ description })
+    }
+
+    return { updateDescription }
 }
